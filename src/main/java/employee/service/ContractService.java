6ccb9b6346142b31fbfd27913a97a66274339d60
huybui20 <97325296+huybui20@users.noreply.github.com>
2023-05-09 23:37:38 +0700

    Update ContractService.java

--- src/main/java/employee/service/ContractService.java
+++ src/main/java/employee/service/ContractService.java
@@ -3,6 +3,8 @@ package employee.service;
 import employee.model.Contract;
 
 public interface ContractService {
+	List <Contract> getAllContracts();
+	Contract getContractById(long id);
 	void saveContract(Contract contract);
 	void deleteContractById(Long id);
 }
